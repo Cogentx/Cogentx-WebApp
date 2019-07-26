6b45db5c03840ad3b2f052386813dbfd8bd223d1
Cogentx <cogentx@gmail.com>
2019-07-26 19:42:34 -0230

    tutorial-routing-storage-themes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    // RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes),
   ],
   exports: [RouterModule]
 })
--- src/app/tutorial/tutorial.page.ts
+++ src/app/tutorial/tutorial.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+import { IonSlide, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tutorial',
   templateUrl: './tutorial.page.html',
   styleUrls: ['./tutorial.page.scss'],
 })
-export class TutorialPage implements OnInit {
+export class TutorialPage {
+  constructor(private storage: Storage, private router: Router) {}
 
-  constructor() { }
+  @ViewChild('slides') slides: IonSlides;
 
-  ngOnInit() {
+  async finish() {
+    await this.storage.set('tutorialComplete', true);
+    this.router.navigateByUrl('/');
   }
 
+  next() {
+    this.slides.slideNext();
+  }
 }
